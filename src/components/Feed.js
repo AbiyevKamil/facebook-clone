5e7ab8820a8a45082073cbf57711e9b764e2ae87
habil99 <abiyevhabil3@gmail.com>
2021-03-10 14:56:33 +0400

    all components done

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className={"feed"}>
       <StoryReel />
       <MessageSender />
+      <Post />
     </div>
   );
 }
--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -1,39 +1,16 @@
 import React from "react";
 import Story from "./Story";
-import image from '../';
-import '../styles/StoryReel.css'
-
-const img =
-  "https://bi.im-g.pl/im/0b/a2/18/z25829387V,las--zdjecie-ilustracyjne---Od-3-kwietnia-obowiazu.jpg";
+import image from "../images/pp.jpg";
+import "../styles/StoryReel.css";
 
 function StoryReel() {
   return (
     <div className="story-reel">
-      <Story
-        image={img}
-        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
-        title="Kamil Abiyev"
-      />
-      <Story
-        image={img}
-        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
-        title="Kamil Abiyev"
-      />
-      <Story
-        image={img}
-        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
-        title="Goon Deve3"
-      />
-      <Story
-        image={img}
-        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
-        title="Goon Deve3"
-      />
-      <Story
-        image={img}
-        profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
-        title="Goon Deve3"
-      />
+      <Story image={image} profileSrc={image} title="Kamil Abiyev" />
+      <Story image={image} profileSrc={image} title="Kamil Abiyev" />
+      <Story image={image} profileSrc={image} title="Kamil Abiyev" />
+      <Story image={image} profileSrc={image} title="Kamil Abiyev" />
+      <Story image={image} profileSrc={image} title="Kamil Abiyev" />
     </div>
   );
 }
